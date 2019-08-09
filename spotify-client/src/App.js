ed8e7281a3577b93d7797ccaf207361856d002c8
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-08 22:55:20 -0400

    making fetch request to backend for songs

--- spotify-client/src/App.js
+++ spotify-client/src/App.js
@@ -11,16 +11,37 @@ class App extends Component {
   state = {
     songs: ''
   }
+
   componentDidMount() {
     this.getSongs()
   }
   getSongs = () => {
-    console.log('hi');
+    fetch(`http://localhost:3000/songs`)
+      .then(res => res.json())
+      .then(json => this.setState({
+        songs: json
+        }))
+      .catch(err => console.log(err))
   }
+
   render(){
     return (
       <>
       <p>hello world</p>
+
+      {
+        this.state.songs
+        ? this.state.songs.map((entry,i) => (
+          <img
+            src={`/images/${entry.img}`}
+            style={{height: 200}}
+            key={i}
+          />
+
+        ))
+        : null
+      }
+
 
       <Main />
       <Sidebar />
