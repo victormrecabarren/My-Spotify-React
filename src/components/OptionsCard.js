fcf8de1c7ca3ed489341239e7d5bd3cd88296cc0
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-14 16:55:12 -0400

    set up functions in react client to create playlist, now setting up backend to receive

--- src/components/OptionsCard.js
+++ src/components/OptionsCard.js
@@ -3,11 +3,39 @@ import React, { Component } from 'react'
 class OptionsCard extends Component {
   state = {
     addingToPlaylist: '',
+    playlists: '',
+    creatingPlaylist: false,
+    playlistInput: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.props.selected);
+    this.setState({
+      creatingPlaylist: false,
+      playlists: [{
+        name: 'newplaylist1'
+      }, {
+        name: 'newplaytlist2'
+      }]
+    })
   }
 
   handleHover = (bool) => {
     this.setState({
       addingToPlaylist: true
+    })
+  }
+
+  newPlaylist = (song) => {
+    this.setState({
+      creatingPlaylist: true
     })
   }
 
@@ -54,18 +82,39 @@ class OptionsCard extends Component {
               ? <div
                   className="createPlaylistCard"
                   >
-                    <div
+                    {this.state.creatingPlaylist?
+                      <div>
+                        <form
+                          onSubmit={this.handleSubmit}
+                          className="searchForm"
+                          >
+                          <input
+                            id="playlistInput"
+                            type="text"
+                            value={this.state.playlistInput}
+                            onChange={this.handleChange}
+                            placeholder="My Playlist #5"
+
+                          />
+                        </form>
+                      </div>
+                      :
+                      <div
                       onClick={() => {
-                        console.log('going to create playlist')
+                        this.newPlaylist(this.props.selected)
                       }}>
                       New Playlist
-                    </div>
-                    <div>
-                      createplaylist
-                    </div>
-                    <div>
-                      createplaylist
-                    </div>
+                    </div>}
+                    {
+                      this.state.playlists
+                      ?
+                      <>
+                      {this.state.playlists.map(playlist => (
+                        <div>{playlist.name}</div>
+                      ))}
+                  </>
+                    : null
+                  }
 
                 </div>
               : null
