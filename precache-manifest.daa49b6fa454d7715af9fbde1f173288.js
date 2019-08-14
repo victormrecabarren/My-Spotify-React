26bd7c46ec07557d9bfae9357a1c850864ed5536
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-14 17:50:52 -0400

    Updates

---
+++ precache-manifest.daa49b6fa454d7715af9fbde1f173288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ce94a863505db4608e77e0d13b3aa2",
+    "url": "/My-Spotify-React/index.html"
+  },
+  {
+    "revision": "4bffe13cc18840cdc122",
+    "url": "/My-Spotify-React/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "81b88ecdaf4d193826c6",
+    "url": "/My-Spotify-React/static/css/main.e1ac4a05.chunk.css"
+  },
+  {
+    "revision": "4bffe13cc18840cdc122",
+    "url": "/My-Spotify-React/static/js/2.be54cfc3.chunk.js"
+  },
+  {
+    "revision": "81b88ecdaf4d193826c6",
+    "url": "/My-Spotify-React/static/js/main.b2b950ef.chunk.js"
+  },
+  {
+    "revision": "e754f964f55b59ac8de0",
+    "url": "/My-Spotify-React/static/js/runtime~main.fb7e9b4a.js"
+  }
+]);
