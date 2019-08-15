3f8189e0b77b4af635fdce1765547725723bd067
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-14 20:12:23 -0400

    Updates

--- precache-manifest.cb835b2ac7205f0fc06d2d71b62be83d.js
+++ precache-manifest.cb835b2ac7205f0fc06d2d71b62be83d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7e71463fb3b4fd905575404cfb7e5673",
+    "revision": "4dfbb59a867c46c1987d7b49096f52fd",
     "url": "/My-Spotify-React/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/My-Spotify-React/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "75f7519d6e6575df590f",
+    "revision": "450aca1963479610dd81",
     "url": "/My-Spotify-React/static/css/main.e1ac4a05.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/My-Spotify-React/static/js/2.6f9ecf96.chunk.js"
   },
   {
-    "revision": "75f7519d6e6575df590f",
-    "url": "/My-Spotify-React/static/js/main.84e8aa1f.chunk.js"
+    "revision": "450aca1963479610dd81",
+    "url": "/My-Spotify-React/static/js/main.73a4bb54.chunk.js"
   },
   {
     "revision": "e754f964f55b59ac8de0",
