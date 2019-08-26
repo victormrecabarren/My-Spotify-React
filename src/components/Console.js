2282d617e8930fbfc9db6ac8ecc5fa4bc01569dc
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-26 13:38:16 -0400

    fixed issue with main by adding overflow-y scroll style

--- src/components/Console.js
+++ src/components/Console.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Console extends Component {
   render() {
     return(
-      <h4>
-        Console component!
+      <h4 style={{textAlign: "center", paddingTop: "30px", fontSize: "0.8em"}}>
+        Playback coming soon...
       </h4>
     )
   }
